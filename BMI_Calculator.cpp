c4b9a65888a55f58211140cb003a9e73b7490044
Calvin Truong <calvin.truong31@gmail.com>
2018-09-29 12:35:20 -0700

    Update BMI_Calculator.cpp

--- BMI_Calculator.cpp
+++ BMI_Calculator.cpp
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CalvinTProj1_BMI
+namespace BMI_Calculator_Project
 {
     public partial class Form1 : Form
     {
@@ -19,23 +19,75 @@ namespace CalvinTProj1_BMI
 
         private void btnCompute_Click(object sender, EventArgs e)
         {
-            double feet = Convert.ToDouble(txtFeet.Text);
-            double feetInches = feet * 12;
-            double inches = Convert.ToDouble(txtInches.Text);
-            double newInches = feetInches + inches;
-            double meter = newInches / 39.3701;
-            double lbs = Convert.ToDouble(txtLbs.Text);
-            double kilogram = lbs * 0.453592;
-            double BMI = kilogram / (meter * meter);
-            txtBMI.Text = Math.Round(BMI, 1).ToString();
+            //Feet
+            int feet;
+            string feetInput = txtFeet.Text;
+            bool correctFeet = Int32.TryParse(feetInput, out feet);
+            if (correctFeet && (Convert.ToInt32(feetInput) > 0))
+            {
+                txtFeet.BackColor = SystemColors.Window;
+            }
+            else
+            {
+                MessageBox.Show("Feet input is invalid, please try again");
+                txtFeet.Clear();
+                txtFeet.Focus();
+                txtFeet.BackColor = System.Drawing.Color.Red;
+                txtBMI.Clear();
+            }
+
+            //Inches 
+            int inches;
+            string inchesInput = txtInches.Text;
+            bool correctInches = Int32.TryParse(inchesInput, out inches);
+            if (correctInches && (Convert.ToInt32(inchesInput) > 0))
+            {
+                txtInches.BackColor = SystemColors.Window;
+            }
+            else
+            {
+                MessageBox.Show("Inches input is invalid, please try again");
+                txtInches.Clear();
+                txtInches.Focus();
+                txtInches.BackColor = System.Drawing.Color.Red;
+                txtBMI.Clear();
+            }
+
+            //Weight
+            double lbs;
+            string lbsInput = txtLbs.Text;
+            bool correctLbs = Double.TryParse(lbsInput, out lbs);
+            if (correctLbs && (Convert.ToDouble(lbsInput) > 0))
+            {
+                txtLbs.BackColor = SystemColors.Window;
+            }
+            else
+            {
+                MessageBox.Show("Weight input is invalid, please try again");
+                txtLbs.Clear();
+                txtLbs.Focus();
+                txtLbs.BackColor = System.Drawing.Color.Red;
+                txtBMI.Clear();
+            }
+
+            if (correctFeet && correctInches && correctLbs && feet > 0 && inches > 0 && lbs > 0)
+            {
+                int height = ((12 * feet) + inches);
+                double heightDouble = Convert.ToDouble(height);
+
+                double BMI = (lbs / (heightDouble * heightDouble)) * 703;
+                txtBMI.Text = Math.Round(BMI, 2).ToString();
+            }
         }
+
         private void btnClear_Click(object sender, EventArgs e)
         {
-            txtBMI.Text = "";
-            txtFeet.Text = "";
-            txtInches.Text = "";
-            txtLbs.Text = "";
+            txtFeet.Clear();
+            txtInches.Clear();
+            txtLbs.Clear();
+            txtBMI.Clear();
         }
+
         private void btnExit_Click(object sender, EventArgs e)
         {
             Close();
